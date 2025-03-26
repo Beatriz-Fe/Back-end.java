d9362ca573a8d36f2f19360580cd887ca4188f33
Beatriz-Fe <biaf0949@gmail.com>
2025-03-26 13:51:08 +0000

    Aula01 de Variáveis

---
+++ src/main/java/com/mycompany/Aula01.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+public class Aula01 {
+
+    public static void main(String[] args) {
+       //variavel logica
+       //tipo primitivo
+       Boolean valorLogicoPrimitivo = true;
+
+       //tipo abstrato
+       Boolean valorLogivoAbstrato = false;
+    }
+}
