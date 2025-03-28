5ce388cf39c6427e8be37b11809dc464ddd31df7
Beatriz-Fe <biaf0949@gmail.com>
2025-03-28 13:46:23 +0000

    aula01 sobre variaveis

--- src/main/java/com/mycompany/Aula01.java
+++ src/main/java/com/mycompany/Aula01.java
@@ -5,9 +5,37 @@ public class Aula01 {
     public static void main(String[] args) {
        //variavel logica
        //tipo primitivo
+       
        Boolean valorLogicoPrimitivo = true;
 
+       //exibe o valor
+       System.out.println(valorLogicoPrimitivo);
+
        //tipo abstrato
-       Boolean valorLogivoAbstrato = false;
+       Boolean valorLogicoAbstrato = false;
+
+       System.out.println("O valor da variavel valorLogicoAbstrato é:" +valorLogicoAbstrato);
+
+       //variaveis inteiras
+       //Tipo primitivo
+
+       int valorInteiro = 10; //32bits
+       long valorInteiroLongo = 10L; //64bits
+
+       //valor abstrato
+       Integer valorInteiroAbs = 20;
+       Long valorLongAbs = 20L;
+
+       //tipo primitivo
+       float numeroFloat = 10.5f;
+       double numeroDouble = 10.5f;
+
+       Float numeroFloatAbs = 10.5f;
+       Double numeroDoubleAbs = 10.5;
+       System.out.println("O valor de numeroFloat é: " +numeroFloat+ ", e o valor numeroDouble é: " +numeroDouble);
+
+        
+
+       System.out.println("O valor inteiro é: " + valorInteiro + ", e o valor inteiro longo é: " + valorInteiroLongo);
     }
 }
