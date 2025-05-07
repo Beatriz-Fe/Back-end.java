c89f6b7c557ca15a4cb89560bbc2da2051409169
Beatriz-Fe <biaf0949@gmail.com>
2025-05-07 13:44:18 +0000

    Laços de repetição

---
+++ src/main/java/com/mycompany/Aula03Operadores.java
@@ -0,0 +1,26 @@
+package com.mycompany;
+
+public class Aula03Operadores {
+    public static void main(String[] args) {
+        /**
+         * Tipos de Comentários
+         */
+
+        // Comentário de uma linha
+
+        /**
+         * Comentários de
+         * múltiplas
+         * linhas
+         */
+
+         /**
+          * Declaração de variáveis
+          */
+
+          String var1; //null
+          String var2 = null; //null
+          String var3 = ""; //vazio
+
+    }
+}
