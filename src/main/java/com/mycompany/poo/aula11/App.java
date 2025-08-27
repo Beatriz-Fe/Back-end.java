ad6f196f44639ac0bbed057a358d118b2d1ec1d3
Beatriz-Fe <biaf0949@gmail.com>
2025-08-27 13:55:17 +0000

    Aula 11, encapsulamento

--- src/main/java/com/mycompany/poo/aula11/App.java
+++ src/main/java/com/mycompany/poo/aula11/App.java
@@ -2,6 +2,13 @@ package com.mycompany.poo.aula11;
 
 public class App {
     public static void main(String[] args) {
-        
+        Pessoa pessoa = new Pessoa();
+
+        //Modificar o valor
+        pessoa.setNome("Bia");
+        pessoa.setIdade(17);
+
+        //acessar o valor
+        System.out.printf("%s tem %d anos de idade\n", pessoa.getNome(), pessoa.getIdade());
     }
 }
--- src/main/java/com/mycompany/poo/aula11/Pessoa.java
+++ src/main/java/com/mycompany/poo/aula11/Pessoa.java
@@ -17,4 +17,39 @@ public class Pessoa {
      private double salario;
      private double altura;
     
+     //Declramos sempre após a declaração dos atributos
+
+     //Método acessor sempre começa com get
+     public String getNome() {
+        return nome;
+     }
+
+     //Método modificador, sempre começa com set
+     public void setNome(String nome) {
+        this.nome = nome;
+     }
+
+      public int getIdade() {
+        return idade;
+     }
+
+     public void setIdade(int idade) {
+        this.idade = idade;
+     }
+
+     public double getSalario() {
+        return salario;
+     }
+
+     public void setSalario(double salario) {
+        this.salario = salario;
+     }
+
+     public double getAltura() {
+        return altura;
+     }
+
+     public void setAltura(double altura) {
+        this.altura = altura;
+     }
 }
