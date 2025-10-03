c68501fc27cd9c68d87ddef7824777ae875e9fd1
Beatriz-Fe <biaf0949@gmail.com>
2025-10-03 14:35:41 +0000

    aula13, herança

---
+++ src/main/java/com/mycompany/poo/aula13/animais/Leao.java
@@ -0,0 +1,7 @@
+package com.mycompany.poo.aula13.animais;
+
+public class Leao extends Animal {
+    public Leao(String nome, double altura, double peso) {
+        super(nome, altura, peso);
+    }
+}
