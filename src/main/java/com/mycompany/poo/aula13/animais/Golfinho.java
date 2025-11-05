2e82f7bc0b8728e5b245321e1cf981b2a711803b
Beatriz-Fe <biaf0949@gmail.com>
2025-11-05 13:48:44 +0000

    aula13

--- src/main/java/com/mycompany/poo/aula13/animais/Golfinho.java
+++ src/main/java/com/mycompany/poo/aula13/animais/Golfinho.java
@@ -5,4 +5,12 @@ public class Golfinho extends Animal {
     public void emitirSom(){
         System.out.println("O golfinho está grunhindo...");
     }
+    @Override
+    public void mover(){
+        System.out.println(this.getNome() + " está nadando...");
+    }
+    @Override
+    public void mover (String direcao){
+        System.out.println(this.getNome + " está nadando para o " + direcao + "...");
+    }
 }
